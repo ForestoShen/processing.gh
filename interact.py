ff4992e4ef7e7a7632af5fd70152d921bf68a54f
zasti <847943216@qq.com>
2017-07-15 13:33:56 +0800

    mouse and keyboard interaction

---
+++ interact.py
@@ -0,0 +1,33 @@
+import ctypes
+import System.Windows.Forms.Keys as Keys
+user32 = ctypes.WinDLL('user32')
+
+_keydict = {"ESC":Keys.Escape,
+           "LSHIFT":Keys.LShiftKey,
+           "RSHIFT":Keys.RShiftKey,
+           "SHIFT":Keys.ShiftKey,
+           "ENTER":Keys.Enter,
+           "SPACE":Keys.Space,
+           "CONTROL":Keys.ControlKey,
+           "CTRL":Keys.ControlKey,
+           "ALT":Keys.Alt,
+           "DOWN":Keys.Down,
+           "UP":Keys.Up,
+           "LEFT":Keys.Left,
+           "RIGHT":Keys.Right,
+           "RETURN":Keys.Enter,
+           "DELETE":Keys.Delete,
+           "CAPS":Keys.Capital,
+           "BACKSPACE":Keys.Back,
+}
+
+def keyPressed(key):
+    try:
+        a = ord(key.upper())
+    except :
+        a = _keydict[key.upper()]
+    keyState = user32.GetKeyState(a)
+    flag = False
+    if ((keyState & 0x8000) != 0):
+        flag = True
+    return flag
